79238e12ffdaf4d21fe6befbd88539daf23ccc5b
Taynara Jacon <tjacon@partnerfusion.com>
2019-05-22 14:39:57 -0300

    Improve to use suite

--- conf.js
+++ conf.js
@@ -3,7 +3,11 @@
 exports.config = {
     directConnect: true,
     framework: 'jasmine2',
-    specs: ['specs/*_spec.js'],
+    // specs: ['specs/*_spec.js'],
+    suites: {
+        calculator: 'specs/ng-calc_spec.js',
+        mark7: 'specs/mark7'
+    },
     // When using this is not possible use angular (ng-calc_spec.js)
     onPrepare: function() {
         browser.ignoreSynchronization = true;
